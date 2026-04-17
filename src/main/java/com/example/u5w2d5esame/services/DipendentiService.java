2c5245ee09b06abbe1d710c31415a0e69f2a0557
Francesco Loffredo <f.loffredo00@gmail.com>
2026-04-17 14:22:59 +0200

    Prove in pgAdmin per dipendenti

---
+++ src/main/java/com/example/u5w2d5esame/services/DipendentiService.java
@@ -0,0 +1,78 @@
+package com.example.u5w2d5esame.services;
+
+import com.example.u5w2d5esame.entities.Dipendente;
+import com.example.u5w2d5esame.exceptions.BadRequestException;
+import com.example.u5w2d5esame.exceptions.NotFoundException;
+import com.example.u5w2d5esame.payloads.DipendenteDTO;
+import com.example.u5w2d5esame.repositories.DipendentiRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class DipendentiService {
+    private final DipendentiRepository dipendentiRepository;
+    public DipendentiService(DipendentiRepository dipendentiRepository) {
+        this.dipendentiRepository = dipendentiRepository;
+    }
+    public Dipendente save(DipendenteDTO body) {
+        if (this.dipendentiRepository.existsByEmail(body.email())) {
+            throw new BadRequestException("L'email " + body.email() + " è già in uso");
+        }
+
+        if (this.dipendentiRepository.existsByUsername(body.username())) {
+            throw new BadRequestException("Lo username " + body.username() + " è già in uso");
+        }
+
+        Dipendente newDipendente = new Dipendente(
+                body.username(),
+                body.nome(),
+                body.cognome(),
+                body.email(),
+                body.password()
+        );
+
+        Dipendente savedDipendente = this.dipendentiRepository.save(newDipendente);
+        log.info("Il dipendente con id {} è stato salvato correttamente", savedDipendente.getId());
+        return savedDipendente;
+    }
+    public Page<Dipendente> findAll(int page, int size, String sortBy) {
+        if (size > 100 || size < 1) size = 10;
+        if (page < 0) page = 0;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        return this.dipendentiRepository.findAll(pageable);
+    }
+    public Dipendente findById(UUID id) {
+        return this.dipendentiRepository.findById(id).orElseThrow(() -> new NotFoundException(id));
+    }
+    public Dipendente findByIdAndUpdate(UUID id, DipendenteDTO body) {
+        Dipendente found = this.findById(id);
+
+        if (!found.getEmail().equals(body.email()) && this.dipendentiRepository.existsByEmail(body.email())) {
+            throw new BadRequestException("L'email " + body.email() + " è già in uso");
+        }
+
+        if (!found.getUsername().equals(body.username()) && this.dipendentiRepository.existsByUsername(body.username())) {
+            throw new BadRequestException("Lo username " + body.username() + " è già in uso");
+        }
+        found.setUsername(body.username());
+        found.setNome(body.nome());
+        found.setCognome(body.cognome());
+        found.setEmail(body.email());
+        found.setPassword(body.password());
+
+        Dipendente updatedDipendente = this.dipendentiRepository.save(found);
+        log.info("Il dipendente con id {} è stato aggiornato correttamente", updatedDipendente.getId());
+        return updatedDipendente;
+    }
+    public void findByIdAndDelete(UUID id) {
+        Dipendente found = this.findById(id);
+        this.dipendentiRepository.delete(found);
+    }
+}
